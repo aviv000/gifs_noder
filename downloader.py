b21314bb06a46764c91dfa14ac822431187a6bae
Shekker <marecosystem394@gmail.com>
2022-04-07 17:23:31 +0300

    added threading

--- downloader.py
+++ downloader.py
@@ -3,7 +3,10 @@ import requests
 import json
 import pathlib
 import time
+from threading import Thread, Lock
 
+
+error_lock = Lock()
 class GifsDownloader:
     def __init__(self, gifs_per_category : int) -> None:
         headers = {
@@ -29,25 +32,32 @@ class GifsDownloader:
             'tagName' : ""
         }
         pathlib.Path("categories").mkdir(parents=True, exist_ok=True)
+        threads = []
         for category in categories:
             pathlib.Path(f"categories/{category}").mkdir(parents=True, exist_ok=True)
             params["tagName"] = category
             response = requests.get('https://api.gfycat.com/v1/reactions/populated',
                                     headers=self.header, params=params)
             for gif_data in response.json()["gfycats"]:
-                gif_url = gif_data["content_urls"]["largeGif"]["url"] if "largeGif" in  gif_data["content_urls"] and "url" in gif_data["content_urls"]["largeGif"] else gif_data["gifUrl"]
-                response = requests.get(gif_url, allow_redirects=True)
-                if response.status_code == 200:
-                    with open(f"categories/{category}/{gif_data['gfyName']}.gif", "wb") as gif_file:
-                        gif_file.write(response.content)
-                    with open(f"categories/{category}/{gif_data['gfyName']}.tags", "w") as tags_file:
-                        tags_file.write(json.dumps(gif_data["tags"]))
-                else:
-                    print(category)
-                    with open("error_gifs", "a+") as error_files:
-                        error_files.write(f"{gif_url}\n")
-            time.sleep(0.3)
-            # print(json.dumps(response.json(), indent=4))
+                threads.append(Thread(target=self.download_gif,args=(gif_data,category,)))
+        
+        for thread in threads:
+            thread.start()
+            thread.join()
+
+    def download_gif(self, gif_data, gif_category):
+        gif_url = gif_data["content_urls"]["largeGif"]["url"] if "largeGif" in  gif_data["content_urls"] and "url" in gif_data["content_urls"]["largeGif"] else gif_data["gifUrl"]
+        response = requests.get(gif_url, allow_redirects=True)
+        if response.status_code == 200:
+            with open(f"categories/{gif_category}/{gif_data['gfyName']}.gif", "wb") as gif_file:
+                gif_file.write(response.content)
+            with open(f"categories/{gif_category}/{gif_data['gfyName']}.tags", "w") as tags_file:
+                tags_file.write(json.dumps(gif_data["tags"]))
+        else:
+            print(gif_category)
+            with error_lock:
+                with open("error_gifs", "a+") as error_files:
+                    error_files.write(f"{gif_url}\n")
 
 def main():
     downloader = GifsDownloader(gifs_per_category=256)
